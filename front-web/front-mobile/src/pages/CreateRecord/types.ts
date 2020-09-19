81b203d6d3cd9eee839e064e81b0f3a90815b305
Carlos Domingues <crdpc@yahoo.com.br>
2020-09-19 12:53:58 -0300

    Ended App on React Native

---
+++ front-web/front-mobile/src/pages/CreateRecord/types.ts
@@ -0,0 +1,14 @@
+import { type } from "os";
+
+export type GamePlatform = 'XBOX' | 'PC' | 'PLAYSTATION';
+
+export type Game = {
+    id: number;
+    title: string;
+    platform: GamePlatform;
+    label: string;
+    value: number;
+    
+}
+
+
